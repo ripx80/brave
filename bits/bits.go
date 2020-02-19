ec990c32a0171b1ea011ba56e6b88b6d3e73618b
ripx80 <ripx80@protonmail.com>
2020-02-19 14:40:52 +0100

    add bits funcs

---
+++ bits/bits.go
@@ -0,0 +1,16 @@
+package bits
+
+// Sets the bit at pos in the integer n.
+func setBit(n int, pos uint) int {
+	n |= (1 << pos)
+	return n
+}
+
+// Clears the bit at pos in n.
+func clearBit(n int, pos uint) int {
+	return n &^ (1 << pos)
+}
+
+func hasBit(n int, pos uint) bool {
+	return (n&(1<<pos) > 0)
+}
